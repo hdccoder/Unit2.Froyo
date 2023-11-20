6a27abe68ffbeceed702cca5e34ec249358f8468
Heather Lockett <heatherlockett@Heathers-MacBook-Pro.local>
2023-11-20 00:17:43 -0500

    I tried but I couldn't figure this one out?

---
+++ index.js
@@ -0,0 +1,4 @@
+const flavors = window.prompt("Please enter Froyo flavors.", "vanilla,vanilla,vanilla,strawberry,coffee,coffee");
+console.log(flavors)
+const flavorsArr = flavors.split(",");
+console.log(flavorsArr);
